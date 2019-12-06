902777c906910794166259d57a352c9f7775b736
Jotaelea <victorval1999@gmail.com>
2019-12-06 14:00:00 +0100

    Animaciones y traduccion home

--- script/Registro-JS.js
+++ script/Registro-JS.js
@@ -2,16 +2,26 @@ $(document).ready(function(){
 
       $('#botonContinuar').on('click',function(){
 
-          var cTitulo = document.forms["form1"]["titulo"].value;
+            var element = document.getElementById("botonTerminar");
           /*
                 AQUI ABRIA QUE PONER QUE SE PUSIERA EL TITULO CORRESPONDIENTE
           */
+         /*if(element.style.margin-top == "3%"){
+            $('#circuloVerde').css("margin-top", "53%");
+            $('#circuloGris').css("margin-top", "53%");
+         }*/
+
           $('#FormularioTitulo').css("visibility", "hidden");
           $('#FormularioCategorias').css("visibility", "visible");
-          $('#circuloVerde').css("margin-top", "75.5%");
+          /*$('#circuloVerde').css("margin-top", "75.5%");
           $('#circuloGris').css("margin-top", "75.5%");
           $('#circuloVerde').css("margin-left", "51.5%");
-          $('#circuloGris').css("margin-left", "46.5%");
+          $('#circuloGris').css("margin-left", "46.5%");*/
+
+          $('#circuloVerde').css("visibility", "hidden");
+          $('#circuloGris').css("visibility", "hidden");
+          $('#circuloVerde2').css("visibility", "visible");
+          $('#circuloGris2').css("visibility", "visible");
       });
 
 });
--- script/home.js
+++ script/home.js
@@ -1,10 +1,155 @@
 $(document).ready(function(){
 
-      $('#u5').on('click',function(){
+      function fade(element) {
+            var op = 1;  // initial opacity
+            var timer = setInterval(function () {
+                if (op <= 0.1){
+                    clearInterval(timer);
+                    element.style.visibility = 'hidden';
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op -= op * 0.3;
+            }, 50);
+      }
 
-            $('#dispositivos').css("visibility", "visible");
+      function unfade(element) {
+            var op = 0.1;  // initial opacity
+            element.style.visibility = 'visible';
+            var timer = setInterval(function () {
+                if (op >= 1){
+                    clearInterval(timer);
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op += op * 0.2;
+            }, 10);
+      }
 
+      /************* UNFADE MODIFICADOS *************/
+
+      function unfade03(element) {
+            var op = 0.1;  // initial opacity
+            element.style.visibility = 'visible';
+            var timer = setInterval(function () {
+                if (op >= 1){
+                    clearInterval(timer);
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op += op * 0.3;
+            }, 10);
+      }
+
+      function unfade02(element) {
+            var op = 0.1;  // initial opacity
+            element.style.visibility = 'visible';
+            var timer = setInterval(function () {
+                if (op >= 1){
+                    clearInterval(timer);
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op += op * 0.2;
+            }, 10);
+      }
+
+      function unfade01(element) {
+            var op = 0.1;  // initial opacity
+            element.style.visibility = 'visible';
+            var timer = setInterval(function () {
+                if (op >= 1){
+                    clearInterval(timer);
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op += op * 0.1;
+            }, 10);
+      }
+
+      /************* FADE MODIFICADOS *************/
+
+      function fade01(element) {
+            var op = 1;  // initial opacity
+            var timer = setInterval(function () {
+                if (op <= 0.1){
+                    clearInterval(timer);
+                    element.style.visibility = 'hidden';
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op -= op * 0.1;
+            }, 50);
+      }
+
+      function fade02(element) {
+            var op = 1;  // initial opacity
+            var timer = setInterval(function () {
+                if (op <= 0.1){
+                    clearInterval(timer);
+                    element.style.visibility = 'hidden';
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op -= op * 0.2;
+            }, 50);
+      }
+
+      function fade03(element) {
+            var op = 1;  // initial opacity
+            var timer = setInterval(function () {
+                if (op <= 0.1){
+                    clearInterval(timer);
+                    element.style.visibility = 'hidden';
+                }
+                element.style.opacity = op;
+                element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op -= op * 0.3;
+            }, 50);
+      }
+
+
+      /************* FUNCIONES ONCLICK *************/
+
+
+      $('#areaDispositivos').on('click',function(){
+
+            var element = document.getElementById('dispositivos');
+
+            if (element.style.visibility == "hidden"){
+                  unfade(element);
+
+            }
+            else {
+                  fade(element);
+            }
+           
+            
       });
+
+
+      $('#areaTraduccion').on('click',function(){
+
+            var element1 = document.getElementById('ingless');
+            var element2 = document.getElementById('francess');
+            var element3 = document.getElementById('alemann');
+
+            if (element1.style.visibility == "hidden" && element2.style.visibility == "hidden" && element3.style.visibility == "hidden" ){
+                  unfade03(element1);
+                  unfade01(element2);
+                  unfade02(element3);
+
+            }
+            else {
+                  fade01(element1);
+                  fade03(element2);
+                  fade02(element3);
+            }
+           
+            
+      });
+
+      
 });
 
 
